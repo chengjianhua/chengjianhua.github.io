9fd8fb26369ef03bb1588621e5ec17a9877dc5a6
Jianhua Cheng <jianhua.cheng@liulishuo.com>
2018-04-01 17:23:48 +0800

    init

---
+++ src/components/Bio.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Kyle Mathews`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by Jianhua Cheng who lives and works in Shanghai. Try to build
+          something more attractive and interesting.
+
+          You should follow him on{' '}
+          <a href="https://twitter.com/jianhua_cheng">Twitter</a>{', '}
+          <a href="https://github.com/chengjianhua">Github</a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
